0499f80b1f6d78b15b5ec22d088449e8d7623da6
brain-dead-sodium-chloride <gr277685@gradia.fi>
2023-12-05 18:48:08 +0200

    update

--- scripts.js
+++ scripts.js
@@ -1,15 +1,3 @@
-function toggleSidebar() {
-    var sidebar = document.getElementById('sidebar');
-    var menuIcon = document.querySelector('.menu-icon');
-    if (sidebar.style.width === '250px') {
-        sidebar.style.width = '0';
-        menuIcon.style.right = '10px'; // Adjust the original position
-    } else {
-        sidebar.style.width = '250px';
-        menuIcon.style.right = '260px'; // Adjust the position when the sidebar is open
-    }
-}
-
 function openSidebar() {
     var sidebar = document.getElementById('sidebar');
     sidebar.style.width = '250px';
