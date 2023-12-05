a0e7a7a6a2dc4de07e7feebcd9b2c45bee5f24c5
brain-dead-sodium-chloride <gr277685@gradia.fi>
2023-12-05 18:08:01 +0200

    side bar added

--- scripts.js
+++ scripts.js
@@ -1,23 +1,32 @@
-document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('scrollToBottomButton').addEventListener('click', function() {
-        // Scroll to the bottom of the page
-        window.scrollTo({
-            top: document.body.scrollHeight,
-            behavior: 'smooth'
-        });
-    });
-});
+function toggleSidebar() {
+    var sidebar = document.getElementById('sidebar');
+    var menuIcon = document.querySelector('.menu-icon');
+    if (sidebar.style.width === '250px') {
+        sidebar.style.width = '0';
+        menuIcon.style.right = '10px'; // Adjust the original position
+    } else {
+        sidebar.style.width = '250px';
+        menuIcon.style.right = '260px'; // Adjust the position when the sidebar is open
+    }
+}
 
+function openSidebar() {
+    var sidebar = document.getElementById('sidebar');
+    sidebar.style.width = '250px';
+}
 
-document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('scrollToTopButton').addEventListener('click', function() {
-        // Scroll to the top of the page
-        window.scrollTo({
-            top: 0,
-            behavior: 'smooth'
-        });
-    });
-});
+function closeSidebar() {
+    var sidebar = document.getElementById('sidebar');
+    sidebar.style.width = '0';
+}
 
+function scrollToBottom(event) {
+    // Prevent the default behavior of the link
+    event.preventDefault();
 
+    // Get the target element
+    var targetElement = document.querySelector('.contact-info');
 
+    // Scroll to the target element
+    targetElement.scrollIntoView({ behavior: 'smooth' });
+  }
