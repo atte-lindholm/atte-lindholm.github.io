0d59ba466a234b0ab67e5b81cad956caf61c5da3
brain-dead-sodium-chloride <gr277685@gradia.fi>
2023-12-05 13:29:29 +0200

    new scripts file

--- scrtipt.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('scrollToBottomButton').addEventListener('click', function() {
-    // Scroll to the bottom of the page
-    window.scrollTo({
-        top: document.body.scrollHeight,
-        behavior: 'smooth'
-    });
-});
